3350a01dec32c4d63943cb67450e50c3c84f85d1
Brandon Felty <brcfelty@gmail.com>
2022-07-29 09:28:56 -0600

    completed blockaway function

--- taxicab-geometry.js
+++ taxicab-geometry.js
@@ -4,13 +4,62 @@ const blocksAway = function(directions) {
     north: 0
   };
 
+  let facing =  "north";
+  if (directions[0] === "left") {
+    facing = "east";
+  }
+
   for (let i = 0; i < directions.length; i++) {
     if (directions[i] === "right") {
-      coordinates.east += directions[i + 1];
-    } else if (directions[i] === "left") {
-      coordinates.east -+ directions[i + 1];
+      switch (facing) {
+        case 'north':
+          coordinates.east += directions[i + 1];
+          facing = 'east';
+          break;
+        case 'east': 
+          coordinates.north -= directions[i + 1];
+          facing = 'south';
+          break;
+        case 'south': 
+          coordinates.east -= directions[i + 1];
+          facing = 'west';
+          break;
+        case 'west': 
+          coordinates.north += directions[i + 1];
+          facing = 'north';
+          break;
+        default:
+          console.log("please use directions")
+      }
+    } 
+    if (directions[i] === "left") {
+      switch (facing) {
+        case 'north':
+          coordinates.east -= directions[i + 1];
+          facing = 'west';
+          break;
+        case 'east': 
+          coordinates.north += directions[i + 1];
+          facing = 'north';
+          break;
+        case 'south': 
+          coordinates.east += directions[i + 1];
+          facing = 'east';
+          break;
+        case 'west': 
+          coordinates.north -= directions[i + 1];
+          facing = 'south';
+          break;
+        default:
+          console.log("please use directions")
+      }
     } 
   }
   
   return coordinates;
 }
+
+
+console.log(blocksAway(["right", 2, "left", 3, "left", 1]));
+console.log(blocksAway(["left", 1, "right", 1, "left", 1, "right", 1, "left", 1, "right", 1]));
+console.log(blocksAway(["left", 3, "right", 1, "right", 3, "right", 1]));
